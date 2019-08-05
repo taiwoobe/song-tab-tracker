c959b06ecaee70572c5c90d264be8f28c1428918
Taiwo <taiwo.ibidapoobe@gmail.com>
2019-08-05 21:46:21 +0100

    setting up registration form with vuetify

--- client/src/components/register.vue
+++ client/src/components/register.vue
@@ -1,26 +1,28 @@
 <template>
-    <div class="loginSection">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 offset-md-3 loginForm">
-                    <form @submit.prevent="registerUser">
-                        <h2>Regstration Page</h2>
-                        <label for="email"> Email Address: </label>
-                        <input type="text" name="email" placeholder="Enter Email Address" v-model="email">
-                        <br>
-                        <label for="password"> Password: </label>
-                        <input type="password" name="password" placeholder="Enter Password" v-model="password">
-                        <br>
-                        <div class="spinner-holder">
-                            <button class="" type="submit">Register</button>
-                            <p v-if="error"> {{ error }}</p>
-                        </div>
-
-                     </form>
-                </div>
-            </div>
-        </div>
-    </div>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+              <v-toolbar color="primary" dark flat>
+                <v-toolbar-title>Registration form</v-toolbar-title>
+                <v-spacer></v-spacer>
+              </v-toolbar>
+              <v-card-text>
+                <v-form @submit.prevent="registerUser">
+                  <v-text-field label="Email Address" name="register" prepend-icon="email" type="email" v-model="email"></v-text-field>
+                  <v-text-field id="password" label="Password" name="password" prepend-icon="lock" type="password" v-model="password"></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" type="submit" @click="registerUser"> Register</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
 </template>
 
 <script>
@@ -54,46 +56,8 @@ import AuthenticationService from '../services/authenticationService'
 </script>
 
 <style lang="scss" scoped>
-.loginSection {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-size: cover;
-    background-image: url('../assets/blue-bg.png');
-}
-.loginForm {
-    box-shadow: 0 0 35px rgba(0, 0, 0, 0.2);
-    padding: 35px 70px 35px 70px;
-    background: aliceblue;
-    border-radius: 5px;
-    h2 {
-        font-size: 1.7em;
-        font-weight: bold;
-        text-align: center;
-        margin-bottom: 40px;
-    }
-    label {
-        color: rgb(134, 131, 131);
-        margin-bottom: 10px;
-        font-size: 14px;
-        display: block;
-    }
-    input {
-        width: 100%;
-        height: 40px;
-        padding: 0px 5px;
-        margin-bottom: 20px;
-    }
-}
 .spinner-holder {
     display: flex;
     align-items: center;
-}
-@media (max-width: 767px) {
-    .loginForm {
-        margin: 15px;
-        padding: 35px 15px;
-    }
 }
 </style>
