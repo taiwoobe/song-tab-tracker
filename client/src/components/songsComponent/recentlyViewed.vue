658838dc625d0de17c575c6e9a825af51bbac418
Taiwo <taiwo.ibidapoobe@gmail.com>
2019-08-21 14:31:15 +0100

    basic setup for recently viewed panel

---
+++ client/src/components/songsComponent/recentlyViewed.vue
@@ -0,0 +1,37 @@
+<template>
+    <shared-panel title="Recently Viewed Songs">
+        <v-card class="card-body">
+            <v-data-table :headers="headers" :items="recentlyViewed" class="elevation-1"></v-data-table>
+        </v-card>
+    </shared-panel>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+    export default {
+        data() {
+            return {
+                headers: [
+                    {
+                        title: 'Dessert',
+                        artist: 'left',
+                        value: 'name',
+                    },
+                    { text: 'Calories', value: 'calories' }
+                ],
+                recentlyViewed: [
+
+                ]       
+            }
+        }, 
+        computed: {
+           ...mapState([
+               'isUserLoggedIn'
+           ])
+        },     
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
