fae03f21576fa0b5fed1507470e90c75e0157a33
Taiwo <taiwo.ibidapoobe@gmail.com>
2019-08-21 17:12:16 +0100

    column width when logged in

--- client/src/components/songsComponent/songs.vue
+++ client/src/components/songsComponent/songs.vue
@@ -1,19 +1,19 @@
 <template>
     <div>
         <v-content>
-            <v-container fluid>
-                <v-layout>
-                    <v-flex xs12 sm6 md6 class="mx-2">
+            <v-container grid-list-xl>
+                <v-layout :class="{'justify-center': !isUserLoggedIn}">
+                    <v-flex :class="{ md10: !isUserLoggedIn, sm12: isUserLoggedIn, lg6: isUserLoggedIn}" class="mx-2">
                         <song-search></song-search>
                     </v-flex>
                 </v-layout>
             </v-container>
-            <v-container fluid>
-                <v-layout justify-center>
-                    <v-flex xs12 sm6 md6 class="mx-2">
+            <v-container grid-list-xl>
+                <v-layout wrap justify-center>
+                    <v-flex :class="{ md10: !isUserLoggedIn, sm12: isUserLoggedIn, lg6: isUserLoggedIn}">
                         <songs-panel></songs-panel>
                     </v-flex>
-                    <v-flex xs12 sm6 md6>
+                    <v-flex :class="{ md10: !isUserLoggedIn, sm12: isUserLoggedIn, lg6: isUserLoggedIn}"  v-if="isUserLoggedIn">
                         <recently-viewed-songs></recently-viewed-songs>
                     </v-flex>
                 </v-layout>
@@ -26,6 +26,8 @@
 import songSearch from './songSearch'
 import songsPanel from './songPanel'
 import recentlyViewedSongs from './recentlyViewedSongs'
+import {mapState} from 'vuex'
+
     export default {
         components: {
             songSearch,
@@ -35,7 +37,12 @@ import recentlyViewedSongs from './recentlyViewedSongs'
         data() {
             return {
             }
-        }
+        },
+        computed: {
+           ...mapState([
+               'isUserLoggedIn'
+           ])
+        },
     }
 </script>
 
